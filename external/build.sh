d06a4c6550b4fa5736ee99c0dc01140abaf8c416
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-03-04 17:59:13 +0100

    wrote local build sript wrapper

---
+++ external/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Assuming this script is in the external/ folder, we check into it
+#   so we can execute it from anywhere.
+external_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $external_dir
+
+# Remove trailing slash form the article folder.
+article_name=${1%/}
+
+# Locally build the article.
+cd "$article_name/"
+../authorea-scripts/local_build.py --no-build --flatten
+cd $external_dir
+
+# Move the output and clean up.
+cp "$1/authorea_build/authorea_paper.tex" "$article_name.tex"
+rm -rf authorea_build
